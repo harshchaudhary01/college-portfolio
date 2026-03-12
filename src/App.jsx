59581ae69fb86160748d271aa726622a09ac9ec7
harshchaudhary01 <harshcareer01@gmail.com>
2026-03-12 15:21:33 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from "./components/Hero"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import Certifications from "./components/Certifications"
+import University from "./components/University"
+import Achievements from "./components/Achievements"
+import Resume from "./components/Resume"
+import Footer from "./components/Footer"
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Certifications />
+      <University />
+      <Achievements />
+      <Resume />
+      <Footer />
+    </>
+  )
+}
+
+export default App
